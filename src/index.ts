86dafe916529272999697517edaf6c10641d46bc
bipproduction <bip.production.js@gmail.com>
2024-07-25 14:19:48 +0800

    buat server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export { }
+import Aedes from "aedes";
+import { createServer } from "aedes-server-factory";
+
+const aedes = new Aedes();
+const port = process.env.PORT || 8888;
+
+// Membuat server HTTP dengan dukungan WebSocket
+const httpServer = createServer(aedes, { ws: true });
+
+// Menjalankan server
+httpServer.listen(port, () => {
+  console.log(`WebSocket server listening on port ${port}`);
+});
